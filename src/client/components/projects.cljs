b666df5659759d10d77ca3ce10cd08b022efa5c7
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-05-25 23:36:20 -0400

    the minimalajax code necessary for a page displaying my github projects.

---
+++ src/client/components/projects.cljs
@@ -0,0 +1,17 @@
+(ns client.components.projects
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+
+(def github-api-url "https://api.github.com")
+
+(defn fetch-github-projects [user]
+  (go
+   (let [resp (<! (http/get (str github-api-url "/users/" user "/repos")
+                            {:with-credentials? false}))]
+     (map (fn [repo]
+           {:url (:html_url repo)
+            :description (:description repo)
+            :name (:name repo)
+            :last-updated (:pushed_at repo)})
+          (:body resp)))))
