7c34ce0b5b1a79c5dca37e38dbc77fd2f2f164f3
Ian McGaunn <ian@cloudzero.com>
2018-07-09 22:54:06 -0400

    Add some test blog posts for my personal convenience.

--- dev/user.clj
+++ dev/user.clj
@@ -2,31 +2,9 @@
   (:require
    [figwheel-sidecar.repl-api :as f]))
 
-;; user is a namespace that the Clojure runtime looks for and
-;; loads if its available
-
-;; You can place helper functions in here. This is great for starting
-;; and stopping your webserver and other development services
-
-;; The definitions in here will be available if you run "lein repl" or launch a
-;; Clojure repl some other way
-
-;; You have to ensure that the libraries you :require are listed in your dependencies
-
-;; Once you start down this path
-;; you will probably want to look at
-;; tools.namespace https://github.com/clojure/tools.namespace
-;; and Component https://github.com/stuartsierra/component
-
-
 (defn fig-start
   "This starts the figwheel server and watch based auto-compiler."
   []
-  ;; this call will only work are long as your :cljsbuild and
-  ;; :figwheel configurations are at the top level of your project.clj
-  ;; and are not spread across different lein profiles
-
-  ;; otherwise you can pass a configuration into start-figwheel! manually
   (f/start-figwheel!))
 
 (defn fig-stop
@@ -34,9 +12,18 @@
   []
   (f/stop-figwheel!))
 
-;; if you are in an nREPL environment you will need to make sure you
-;; have setup piggieback for this to work
 (defn cljs-repl
   "Launch a ClojureScript REPL that is connected to your build and host environment."
   []
   (f/cljs-repl))
+
+(def test-blog-post-1 {:title "nicetitle", :date "Mon Jul 09 2018", :content "this is a great post"})
+(def test-blog-post-2 {:title "nice titler again",
+                       :date "Mon Jul 09 2018",
+                       :content "this is a great post and I think it's even nicer than some other posts that I have seen in the store\n"})
+(def test-blog-post-3 {:title "nice titles are my middle name",
+                       :date "Mon Jul 09 2018",
+                       :content "this is a great post and I think it's even nicer than some other posts that I have seen in the store\n"})
+(def all-test-posts [test-blog-post-1
+                     test-blog-post-2
+                     test-blog-post-3])
